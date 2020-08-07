d188f3ea7eff75f52c72e153304886e7e23e870e
Vovan1608 <vovan1608@gmail.com>
2020-08-07 14:24:30 +0300

    Added practise Data_conversion

---
+++ #5_Data_conversion/data_conversion.js
@@ -0,0 +1,18 @@
+﻿var x = 1;
+var y = 2;
+
+var res1 = x + String(y);
+console.log(res1); 
+console.log(typeof res1); 
+
+var res2 = Boolean(x) + String(y);
+console.log(res2); 
+console.log(typeof res2);
+
+var res3 = Boolean(x + y);
+console.log(res3); 
+console.log(typeof res3);
+
+var res4 = Math.sqrt(x - y);
+console.log(res4); 
+console.log(typeof res4); 
