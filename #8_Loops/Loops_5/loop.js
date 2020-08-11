0c9a94cdbf599a4be676676f4969f8bcc02bd6fb
Vovan1608 <vovan1608@gmail.com>
2020-08-11 14:33:30 +0300

    solved task 5

---
+++ #8_Loops/Loops_5/loop.js
@@ -0,0 +1,10 @@
+﻿var myArr = [];
+var count = 0;
+for(var i = 0; ; i++){
+    myArr[i] = Number(prompt("Enter next number for array.", ""));
+    if(myArr[i] == "" || isNaN( myArr[i] ) == true){
+        break;
+    } 
+    count += myArr[i];
+}
+console.log("Sum of all numbers of array is: " + count);
