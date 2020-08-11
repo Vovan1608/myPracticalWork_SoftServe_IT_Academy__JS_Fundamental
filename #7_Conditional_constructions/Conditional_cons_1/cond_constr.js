0925b62bf3aa905cae658c367c41401bbc9aa8d3
Vovan1608 <vovan1608@gmail.com>
2020-08-11 09:04:07 +0300

    added directory #7

---
+++ #7_Conditional_constructions/Conditional_cons_1/cond_constr.js
@@ -0,0 +1,10 @@
+﻿var integerNum = Number( prompt ("Enter the integer number.", "0") );
+if(integerNum > 0){
+    console.log("To your number added 1 and it becomes: " + (integerNum + 1) );
+}
+if(integerNum < 0){
+    console.log("Subtract 2 from your number and it becomes: " + (integerNum - 2) );
+}
+if(integerNum == 0){
+    console.log("Your number becomes: " + (integerNum = 10) );;
+}
---
+++ #7_Conditional_constructions/Conditional_cons_2/cond_constr.js
@@ -0,0 +1,12 @@
+﻿var integerNum = Number( prompt ("Enter the integer number.", "0") );
+if(integerNum == 0){
+    console.log("Your number is zero.");
+}else if(integerNum > 0 && integerNum % 2 == 0){
+    console.log("Your number is  positive even.");
+}else if(integerNum > 0 && integerNum % 2 != 0){
+    console.log("Your number is positive odd.");
+}else if(integerNum < 0 && integerNum % 2 == 0){
+    console.log("Your number is negative even.");
+}else if(integerNum < 0 && integerNum % 2 != 0){
+    console.log("Your number is negative odd.");
+}
