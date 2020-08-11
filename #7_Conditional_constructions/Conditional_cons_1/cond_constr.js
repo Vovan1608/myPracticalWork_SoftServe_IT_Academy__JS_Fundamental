921a0244cee19f233528eca591c50fb6ae6e3af3
Vovan1608 <vovan1608@gmail.com>
2020-08-11 10:30:27 +0300

    added else in condi..._1

--- #7_Conditional_constructions/Conditional_cons_1/cond_constr.js
+++ #7_Conditional_constructions/Conditional_cons_1/cond_constr.js
@@ -1,10 +1,8 @@
 ﻿var integerNum = Number( prompt ("Enter the integer number.", "0") );
 if(integerNum > 0){
     console.log("To your number added 1 and it becomes: " + (integerNum + 1) );
-}
-if(integerNum < 0){
+}else if(integerNum < 0){
     console.log("Subtract 2 from your number and it becomes: " + (integerNum - 2) );
-}
-if(integerNum == 0){
-    console.log("Your number becomes: " + (integerNum = 10) );;
+}else if(integerNum == 0){
+    console.log("Your number becomes: " + (integerNum = 10) );
 }
