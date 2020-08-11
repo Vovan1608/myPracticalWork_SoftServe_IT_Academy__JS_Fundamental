325791c00670c99db5d49f298706634f5b429325
Vovan1608 <vovan1608@gmail.com>
2020-08-11 12:59:18 +0300

    solved task 2, added dir loops_3

---
+++ #8_Loops/Loops_3/loop.js
@@ -0,0 +1,7 @@
+﻿var intN = 123456789012345;
+var divider = 10;
+while( (intN / divider) > 0) {
+    numComponents = intN % divider;
+    console.log(numComponents);
+    intN = (intN - numComponents) / divider;
+}
