de29eb61fd822faea1dc617c6f3c5d61a7e7b8d1
Vovan1608 <vovan1608@gmail.com>
2020-08-11 19:08:07 +0300

    solved task 4

---
+++ #9_Functions/Functions_4/func.js
@@ -0,0 +1,16 @@
+﻿function calc(a, b, op){
+    switch(op){
+        case 1:
+            return a - b;
+        case 2:
+            return a * b;
+        case 3:
+            return a / b;
+        default:
+            return a + b;
+    }
+}
+console.log("res is " + calc(2, 3, 1));
+console.log("res is " + calc(4, 5, 2));
+console.log("res is " + calc(-8, 4, 3));
+console.log("res is " + calc(-2, 5, 4));
