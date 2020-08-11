3c1d6c113508afdeb27999a80a7d0e0996416ace
Vovan1608 <vovan1608@gmail.com>
2020-08-11 09:54:00 +0300

    added task 4 for conditional_constructions

---
+++ #7_Conditional_constructions/Conditional_cons_4/cond_constr.js
@@ -0,0 +1,20 @@
+﻿var grade = Number(prompt("Enter the integer number.",""));
+switch(grade){
+    case 1:
+        console.log("badly");
+        break;
+    case 2:
+        console.log("unsatisfactory");
+        break;
+    case 3:
+        console.log("satisfactory");
+        break;
+    case 4:
+        console.log("good");
+        break;
+    case 5:
+        console.log("perfectly");
+        break;
+    default:
+        console.log("Error");
+}
