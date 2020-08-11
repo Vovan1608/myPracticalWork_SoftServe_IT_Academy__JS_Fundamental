95146e3d8c999fddf7634f6147b047b6946a3261
Vovan1608 <vovan1608@gmail.com>
2020-08-11 11:00:23 +0300

    removed the last if in con..._1

--- #7_Conditional_constructions/Conditional_cons_1/cond_constr.js
+++ #7_Conditional_constructions/Conditional_cons_1/cond_constr.js
@@ -3,6 +3,6 @@ if(integerNum > 0){
     console.log("To your number added 1 and it becomes: " + (integerNum + 1) );
 }else if(integerNum < 0){
     console.log("Subtract 2 from your number and it becomes: " + (integerNum - 2) );
-}else if(integerNum == 0){
+}else {
     console.log("Your number becomes: " + (integerNum = 10) );
 }
