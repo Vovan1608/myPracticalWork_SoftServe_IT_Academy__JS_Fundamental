46a856b851113c9f18466a011747556ba2796874
Vovan1608 <vovan1608@gmail.com>
2020-08-11 09:40:24 +0300

    added task 3 for conditional_constructions

---
+++ #7_Conditional_constructions/Conditional_cons_3/cond_constr.js
@@ -0,0 +1,13 @@
+﻿var intA = Number( prompt("Enter integer number a",""));
+var intB = Number( prompt("Enter integer number b",""));
+var intC = Number( prompt("Enter integer number c",""));
+var intD = Number( prompt("Enter integer number d",""));
+if(intA == intB && intB == intC){
+    console.log("Serial number of a different number is 4.");
+}else if(intB == intC && intC == intD){
+    console.log("Serial number of a different number is 1.");
+}else if(intA == intB && intB == intD){
+    console.log("Serial number of a different number is 3.");
+}else if(intA == intC && intC == intD){
+    console.log("Serial number of a different number is 2.");
+}
