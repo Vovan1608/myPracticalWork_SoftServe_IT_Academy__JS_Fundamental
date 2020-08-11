46f6cdf43d85bb205e51912a7928f97c4458ce5f
Vovan1608 <vovan1608@gmail.com>
2020-08-11 18:52:22 +0300

    solved task 3

---
+++ #9_Functions/Functions_3/func.js
@@ -0,0 +1,12 @@
+﻿function sign(x){
+    if(x < 0){
+        return -1;
+    }else if(x > 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+console.log("value is " + sign(5));
+console.log("value is " + sign(0));
+console.log("value is " + sign(-5));
