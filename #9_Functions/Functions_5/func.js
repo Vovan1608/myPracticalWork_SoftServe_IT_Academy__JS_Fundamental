8265c493124a9ca379a34215cc949fb1ba15c095
Vovan1608 <vovan1608@gmail.com>
2020-08-12 17:04:24 +0300

    changed task 5 again. Maybe for the last time

--- #9_Functions/Functions_5/func.js
+++ #9_Functions/Functions_5/func.js
@@ -1,35 +1,23 @@
-﻿function digitN(k, n){
+﻿function digitN(k, n) {
     // 1 
     // определил кол-во цифр в числе
-    // var countDigitK = 0;
-    // const DIVIDER = 10;
-    // var clonK = k;
-    // while( (clonK / DIVIDER) > 0){
-    //     countDigitK++;
-    //     clonK = (clonK - ( clonK % DIVIDER)) / DIVIDER;  
-    // }
-    // // Если количество цифр в числе k меньше n, то функция возвращает -1
-    // if( (countDigitK / n) < 1){
-    //     return -1;
-    // }
-    // // Определил на что будем делить число k (если n=1 то на 10, n=2 то на 100 и т.д.)
-    // const MULTIPLIER = DIVIDER;
-    // var dividerN = DIVIDER;
-    // for(var i = 1; i < n; i++){
-    //     dividerN = dividerN * MULTIPLIER;
-    // }
-    // // Сама цифра
-    // var digitN = parseInt ( (k % dividerN) * MULTIPLIER / dividerN );
-    // return digitN;
-
-    // 2
-    k = String(k);
-    if( (k.length / n) < 1 ){
+    var countDigitK = 0;
+    const TEN = 10;
+    var clonK = k;
+    while ((clonK / TEN) > 0) {
+        countDigitK++;
+        clonK = (clonK - (clonK % TEN)) / TEN;
+    }
+    // Если количество цифр в числе k меньше n, то функция возвращает -1
+    if ((countDigitK - n) < 0) {
         return -1;
     }
-    return k[k.length - n];
+    var dvderK = Math.pow(TEN, n);
+    // Сама цифра
+    var digN = parseInt((k % dvderK) * TEN / dvderK);
+    return digN;
 }
 
-console.log(digitN(95895, 2));
+console.log(digitN(96825, 2));
 console.log(digitN(1234589760213, 10));
-console.log(digitN(95895, 5));
+console.log(digitN(95895, 6));
