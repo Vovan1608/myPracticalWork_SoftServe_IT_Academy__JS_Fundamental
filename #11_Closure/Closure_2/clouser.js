dbf9fb0b3bab48571204bcb6b7a3d978c2cf3a76
Vovan1608 <vovan1608@gmail.com>
2020-08-14 12:21:56 +0300

    solved task 2

---
+++ #11_Closure/Closure_2/clouser.js
@@ -0,0 +1,18 @@
+﻿function calcRactParam(sideA, sideB){
+    if(sideA > 0 && sideB > 0){
+        function calcPerim(){
+            return 2 * (sideA + sideB);
+        }
+
+        function calcArea(){
+            return sideA * sideB;
+        }
+
+        return [calcPerim,calcArea];
+    }
+} 
+
+var res = calcRactParam(4, 5);
+console.log(res[0]());
+console.log(res[1]());
+console.dir(res[0])
