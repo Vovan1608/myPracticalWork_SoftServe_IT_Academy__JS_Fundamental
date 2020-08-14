66663cf2be5e79e418adb11af6ef9b35106d3a15
Vovan1608 <vovan1608@gmail.com>
2020-08-14 09:05:44 +0300

    solved task 1

---
+++ #11_Clousesr/Clouser_1/clouser.js
@@ -0,0 +1,15 @@
+﻿function createArrayIterator(array) {
+    var index = -1;
+    
+    return function (){
+        index++;
+        return arr[index]; 
+    }
+}
+
+var arr = [5, 3, 7];
+var itr = createArrayIterator(arr);
+console.log(itr()); // 5
+console.log(itr()); // 3
+console.log(itr()); // 7
+console.log(itr()); // undefined 
