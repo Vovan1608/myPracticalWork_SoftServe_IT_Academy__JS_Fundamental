97f143314548a0953015bcf22018862fd11d9ef8
Vovan1608 <vovan1608@gmail.com>
2020-08-18 13:24:02 +0300

    solved task 13.1

---
+++ #13_Overriding_func/overrid.js
@@ -0,0 +1,34 @@
+﻿function format (data, type) {
+    if(type === 'number'){
+        return format = function() {
+            data = Number(data);
+            return data;
+        }   
+    }else if(type === 'string'){
+        return format = function(){
+            data = String(data);
+            return data;
+        }
+    }else if(type === 'boolean'){
+        return format = function(){
+            data = Boolean(data);
+            return data;
+        }
+    }else{
+        return data;
+    }
+}
+
+var originFormat = format;
+
+originFormat("1", "number");
+console.log(format()); // 1
+console.log(typeof format()); // "number"
+
+originFormat("Hello", "boolean");
+console.log(format());// true
+console.log(typeof format());// "boolean"
+
+originFormat(true, "string");
+console.log(format());// "true"
+console.log(typeof format()); // "string"
