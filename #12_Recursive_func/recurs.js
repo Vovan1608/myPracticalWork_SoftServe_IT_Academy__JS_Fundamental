5fed0d0f9e114f0ded69fd12623a11e0bb9906fb
Vovan1608 <vovan1608@gmail.com>
2020-08-18 11:59:19 +0300

    solved task 12.1

---
+++ #12_Recursive_func/recurs.js
@@ -0,0 +1,9 @@
+﻿function digitSum(k){
+    const TEN = 10;
+    if( k / TEN < 1 ){
+        return k;
+    }else{
+        return  (k % TEN) + digitSum( parseInt(k / TEN) );
+    }
+}
+console.log(digitSum(12345));
