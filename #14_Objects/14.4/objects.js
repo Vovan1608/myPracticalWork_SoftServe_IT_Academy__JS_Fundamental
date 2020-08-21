740af4feb6ae1be894aa23365c9fd52e63c213d9
Vladymyr Zenin <vovan1608@gmail.com>
2020-08-21 16:02:36 +0300

    changed task 14.4

--- #14_Objects/14.4/objects.js
+++ #14_Objects/14.4/objects.js
@@ -1,20 +1,14 @@
 ﻿'use strict';
 
 var mp3 = {
-    model: "Onda",
-    price: 1182,
-    memory: 64,
-    workingTime: 20,
-    slot: "MicroSD",
-    display: "Mono",
     volume: 1,
+    trek: 1,
     volIncrease: function() {
         return volume++;
     },
     volDown: function(){
         return volume--;
     },
-    trek: 1,
     forvard: function(){
         return trek++;
     },
