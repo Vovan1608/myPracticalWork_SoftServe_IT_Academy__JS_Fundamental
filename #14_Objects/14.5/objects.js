0c97dd9185154bf7a8409b7fced1e3b4ffc2bf05
Vladymyr Zenin <vovan1608@gmail.com>
2020-08-21 16:24:22 +0300

    solved task 14.5

--- #14_Objects/14.5/objects.js
+++ #14_Objects/14.5/objects.js
@@ -1,2 +1,29 @@
 ﻿'use strict';
 
+var company ={
+    employees: [
+        {
+            name: "John",
+            age: 28,
+            experience: 1,
+            languages: ["Java", "JavaScript", "SQL"]
+        },
+
+        {
+            name: "Bill",
+            age: 30,
+            experience: 5,
+            languages: ["HTML", "CSS", "JavaScript"]
+        },
+
+        {
+            name: "Mike",
+            age: 32,
+            experience: 4,
+            languages: ["Python", "SQL"]
+        }
+    ]
+}
+
+console.log(company);
+console.log(company.employees[1].languages[2]);
