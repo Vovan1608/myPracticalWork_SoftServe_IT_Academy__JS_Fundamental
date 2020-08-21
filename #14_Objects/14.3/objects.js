96fd4200ff2407117cd43b7653ac8b60d6d6c81a
Vladymyr Zenin <vovan1608@gmail.com>
2020-08-21 15:31:14 +0300

    solved 14.3

---
+++ #14_Objects/14.3/objects.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+var tv = {
+    currentChannel: 1,
+    nextChannel: function (){
+        return tv.currentChannel++;
+    },
+    previousChannel: function(){
+        return tv.currentChannel--;
+    },
+    setChannel: function (number){
+        return tv.currentChannel = number;
+    }
+}
