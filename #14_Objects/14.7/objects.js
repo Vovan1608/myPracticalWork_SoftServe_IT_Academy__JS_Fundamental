deb004063f687dfe57b931a32eca510241e82cfa
Vovan1608 <vovan1608@gmail.com>
2020-08-22 19:31:15 +0300

    solved task 14.7

---
+++ #14_Objects/14.7/objects.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+var salaries = {
+    John: 100,
+    Bill: 300,
+    Mike: 250
+};
+
+var salaries = {
+    Cris: 150,
+    Brain: 600,
+    John: 300,
+    Steve: 400,
+    Bill: 50
+};
+
+console.log(salaries);
+
+function countAverage(object){
+    var countSalary = 0;
+    var countEmployee = 0;
+    for(var key in object){
+        countSalary +=object[key];
+        countEmployee++;
+    }
+    return countSalary / countEmployee;
+}
+
+console.log(countAverage(salaries));
