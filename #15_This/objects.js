21a7e5bde5c8acfd537e3ad652ac232129e38610
Vovan1608 <vovan1608@gmail.com>
2020-08-23 15:54:59 +0300

    solved task 15.1

--- #15_This/objects.js
+++ #15_This/objects.js
@@ -1,8 +1,24 @@
 ﻿'use strict';
 
 var calculator = {
-    // Ваш код
-};
+
+    firstNum: this.firstNum,
+    secondNum: this.secondNum,
+
+    read: function(){
+        this.firstNum = Number(prompt("Введите первое число", "3"));
+        this.secondNum = Number(prompt("Введите второе число", "5"));
+    },
+
+    sum: function(){
+        return this.firstNum + this.secondNum;
+    },
+
+    mul: function(){
+        return this.firstNum * this.secondNum;
+    },
+}
+
 calculator.read();
 console.log(calculator.sum());
 console.log(calculator.mul());
