e5e2b67dc5b4832a10e0ae992aa178ae41678664
Vovan1608 <vovan1608@gmail.com>
2020-08-28 08:02:14 +0300

    added failes 16.1

---
+++ #16_Objects_in_function/Obj_in_func_1/app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+function createMP3(volume, trek){
+    return {
+        volume: volume,
+        trek: trek,
+        volIncrease: function() {
+            return this.volume++;
+        },
+        volDown: function(){
+            return this.volume--;
+        },
+        forvard: function(){
+            return this.trek++;
+        }
+    }
+}
