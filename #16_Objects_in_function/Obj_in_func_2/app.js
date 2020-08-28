8b838bdfb5b2e9759d1e5e310f6de6d28da8fcfa
Vovan1608 <vovan1608@gmail.com>
2020-08-28 08:46:24 +0300

    added failes task 16.2

---
+++ #16_Objects_in_function/Obj_in_func_2/app.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+function getMaxSalarie(salaries){
+    var array = [];
+    for(var key in salaries){
+        array.push(salaries[key]);
+    }
+    var maxSalarie = array[0];
+    for(var i = 1; i < array.length; i++){
+        if(maxSalarie < array[i]){
+            maxSalarie = array[i]
+        }
+    }
+    return maxSalarie;
+}
+
+var salaries = {
+    John: 100,
+    Bill: 300,
+    Mike: 250
+};
+
+var salaries = {
+    Cris: 150,
+    Brain: 600,
+    John: 300,
+    Steve: 400,
+    Bill: 50
+};
+
+var max = getMaxSalarie(salaries);
+console.log(max); // 600
+
