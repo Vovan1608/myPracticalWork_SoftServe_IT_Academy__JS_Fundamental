592edeead452fec9296aa5b13fe0da2ade75717b
Vovan1608 <vovan1608@gmail.com>
2020-08-28 09:18:45 +0300

    added files 17.1

--- #17_Object_constructors/app.js
+++ #17_Object_constructors/app.js
@@ -2,17 +2,17 @@
 
 function PleerMP3(volume, trek){
 
-        this.volume = volume;
-        this.trek = trek;
-        this.volIncrease = function() {
-            this.volume++;
-        };
-        this.volDown = function(){
-            this.volume--;
-        };
-        this.forvard = function(){
-            this.trek++;
-        }
+    this.volume = volume;
+    this.trek = trek;
+    this.volIncrease = function() {
+        this.volume++;
+    };
+    this.volDown = function(){
+        this.volume--;
+    };
+    this.forvard = function(){
+        this.trek++;
+    }
 }
 
 var sony = new PleerMP3(5, 3);
