2efefb709e24285d8771c22f48c5951b758a184d
Vovan1608 <vovan1608@gmail.com>
2020-08-28 17:27:55 +0300

    remove all returns

--- #17_Object_constructors/app.js
+++ #17_Object_constructors/app.js
@@ -5,19 +5,15 @@ function PleerMP3(volume, trek){
     this.volume = volume;
     this.trek = trek;
     this.volIncrease = function() {
-        return this.volume++;
+        this.volume++;
     };
     this.volDown = function(){
-        return this.volume--;
+        this.volume--;
     };
     this.forvard = function(){
-        return this.trek++;
+        this.trek++;
     }
 }
 
 var sony = new PleerMP3(5, 3);
 console.log(sony);
-sony.volIncrease();
-sony.volIncrease();
-sony.volIncrease();
-console.log(sony.volIncrease());
