743737aa2c39c9a78259df417f437739424022aa
Vovan1608 <vovan1608@gmail.com>
2020-08-28 19:09:23 +0300

    notification in 19.1

--- #19_Context_binding/19_1/app.js
+++ #19_Context_binding/19_1/app.js
@@ -7,11 +7,15 @@ function format(beginMsg, endMsg) {
     console.log(beginMsg + this.name + endMsg);
 }
 
+
+// Function
 user.func = format;
 var tomFormat = function(){
     user.func.call(user, "<<<", ">>>");
 };
 tomFormat("<<<", ">>>");
 
+
+// bind()
 // var tomFormat = format.bind(user);
 // tomFormat ("<<<", ">>>"); // "<<<Tom>>>"
