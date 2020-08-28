874efe5918503f51f52bad54644553227bf4af96
Vovan1608 <vovan1608@gmail.com>
2020-08-28 19:07:38 +0300

    added call() and params

--- #19_Context_binding/19_1/app.js
+++ #19_Context_binding/19_1/app.js
@@ -6,9 +6,10 @@ var user = {
 function format(beginMsg, endMsg) {
     console.log(beginMsg + this.name + endMsg);
 }
+
 user.func = format;
 var tomFormat = function(){
-    user.func;
+    user.func.call(user, "<<<", ">>>");
 };
 tomFormat("<<<", ">>>");
 
