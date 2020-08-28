5d2a4ad7ff0de69f9b4a295dd7531d900f436123
Vovan1608 <vovan1608@gmail.com>
2020-08-28 11:15:59 +0300

    added solve task18.2

---
+++ #18_Function_as_an_object/18_2/app.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+var country = {
+    name: "Украина",
+    language: "украинский",
+    capital: {
+        name: "Киев",
+        population: 2907817,
+        area: 847.66
+    }
+};
+function format(beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
+}
+ format.call(country, "<", ">" ); // "<Украина>"
+ format.apply(country, ["[", "]"]); // "[Украина]"
+ format.call(country.capital, '"', '"'); // ""Киев""
+ format.apply(country.capital, ["", ""]); // "Киев"
