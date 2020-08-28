db02d5c96c48a7e450d939221415ce6bb2b58e73
Vovan1608 <vovan1608@gmail.com>
2020-08-28 10:49:45 +0300

    added solve task 18.1

---
+++ #18_Function_as_an_object/18_1/app.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+function mul() {
+    // Ваш код
+    var res = 1;
+    var count = 0;
+    for (var i = 0; i < arguments.length; i++){
+        if(typeof arguments[i] === "number"){
+            res *= arguments[i];
+            count++;
+        }
+    }
+    if(count > 0){
+        return res;
+    }else{
+        return 0;
+    }
+}
+ console.log(mul(1, "str", 2, 3, true)); // 6
+ console.log(mul(null, "str", false, true)); // 0
