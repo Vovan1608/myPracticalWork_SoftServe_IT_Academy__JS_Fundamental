adafc911acbac864229810ed3d8e66d7bba7e078
Vovan1608 <vovan1608@gmail.com>
2020-09-01 11:12:43 +0300

    added task 20

---
+++ #20_Objects_on_prototypes/app.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+function PleerMP3(volume, trek){
+    this.volume = volume;
+    this.trek = trek;
+}
+PleerMP3.prototype.volIncrease = function(){
+    return ++this.volume;
+}
+PleerMP3.prototype.volDown = function(){
+    return --this.volume;
+}
+PleerMP3.prototype.forvard = function(){
+    return ++this.trek;
+}
+
+var sony = new PleerMP3(5, 1);
+console.log(sony.volIncrease()); // 6
+console.log(sony.volIncrease()); // 7
+console.log(sony.volDown());     // 6
+
+var sumsung = new PleerMP3(1, 3);
+console.log(sumsung.forvard()); // 4
+console.log(sumsung.forvard()); // 5
+console.log(sumsung.forvard()); // 6
