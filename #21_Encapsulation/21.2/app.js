aaa4c9292c8414d3517c864e7a32b9f01168f594
Vovan1608 <vovan1608@gmail.com>
2020-09-02 13:44:28 +0300

    task 21.2

--- #21_Encapsulation/21.2/app.js
+++ #21_Encapsulation/21.2/app.js
@@ -1,32 +1,57 @@
 ﻿'use strict';
 
-function Adder(firstNumber, secondNumber, result){
+function Adder(firstNumber, secondNumber){
+    // 1. Приватное свойство __firstNumber;
     if(this.__isNumber(firstNumber)){
         this.__firstNumber = firstNumber;
     }else{
         this.__firstNumber = 0;
     }
+
+    // 2. Приватное свойство __secondNumber;
     if(this.__isNumber(secondNumber)){
         this.__secondNumber = secondNumber;
     }else{ 
         this.__secondNumber = 0;
     }
-    this.result = result;
+
+    // 3. Публичное свойство result;
+    this.result = 0;
 }
 
+
+Adder.prototype.sum = function(){
+    return function(){
+        return this.__calc();
+    }
+}
+// 4. Приватный метод __calc(), который записывает в result сумму свойств __firstNumber и __secondNumber;
 Adder.prototype.__calc = function(){
     this.result = this.__firstNumber + this.__secondNumber;
-    return this.result;
 }
 
+// 5. методы геттер-сеттер для свойств __firstNumber и __secondNumber
 Adder.prototype.firstNumber = function(firstNumber){
     if(firstNumber === undefined){
         return this.__firstNumber;
-    }else if(this.__isNumber(firstNumber)){
-        this.__firstNumber = firstNumber;
+    }else {
+        if(this.__isNumber(firstNumber)){
+            this.__firstNumber = firstNumber;
+        }
     }
 }
 
+Adder.prototype.secondNumber = function(secondNumber){
+    if(secondNumber === undefined){
+        return this.__secondNumber;
+    }else {
+        if(this.__isNumber(secondNumber)){
+            this.__secondNumber = secondNumber;
+        }
+    }
+}
+
+// валидация
 Adder.prototype.__isNumber = function(number){
     if(typeof number === "number"){
         return true;
@@ -35,21 +60,6 @@ Adder.prototype.__isNumber = function(number){
     }
 }
 
-Adder.prototype.secondNumber = function(secondNumber){
-    if(secondNumber === undefined){
-        return this.__secondNumber;
-    }else if(this.__isNumber(secondNumber)){
-        this.__secondNumber = secondNumber;
-    }
-}
-
 var res = new Adder(5, 10);
-console.log(res.__calc()); // 15 = 5 + 10
-res.firstNumber(10);
-console.log(res.__calc()); // 20 = 10 +10
-res.secondNumber(50);
-console.log(res.__calc()); // 60 = 10 + 50
-res.firstNumber("hju");
-console.log(res.__calc()); // 60 - первый параметр не число
-res.secondNumber(undefined);
-console.log(res.__calc()); // 60 - второй параметр не число 
+
+console.log(res.sum()); // 0 возвращает значение из 19 строки
