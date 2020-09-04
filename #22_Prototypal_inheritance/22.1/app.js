8f58832464f0a0151c7fcb2b4042f777861e9243
Vovan1608 <vovan1608@gmail.com>
2020-09-04 18:07:42 +0300

    added 22.1

---
+++ #22_Prototypal_inheritance/22.1/app.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+function GeometricFigure(centerX, centerY){
+    if(this.__isNumber(centerX)){
+        this.__centerX = centerX;
+    }else{
+        this.__centerX = 0;
+    }
+    if(this.__isNumber(centerY)){
+        this.__centerY = centerY;
+    }else{
+        this.__centerY = 0;
+    }
+    
+}
+
+GeometricFigure.prototype.__isNumber = function (number) {
+    if (typeof number === "number" && number >= 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+GeometricFigure.prototype.setCenterX = function(centerX){
+    if(this.__isNumber(this.__centerX)){
+        this.__centerX = centerX
+    }
+}
+
+GeometricFigure.prototype.getCenterX = function(){
+    return this.__centerX;
+}
+
+GeometricFigure.prototype.setCenterY = function(centerY){
+    if(this.__isNumber(this.__centerY)){
+        this.__centerY = centerY
+    }
+}
+
+GeometricFigure.prototype.getCenterX = function(){
+    return this.__centerY;
+}
---
+++ #22_Prototypal_inheritance/22.2/app.js
@@ -0,0 +1,65 @@
+﻿'use strict';
+
+function Adder(firstNumber, secondNumber){
+    // 1. Приватное свойство __firstNumber;
+    if(this.__isNumber(firstNumber)){
+        this.__firstNumber = firstNumber;
+    }else{
+        this.__firstNumber = 0;
+    }
+
+    // 2. Приватное свойство __secondNumber;
+    if(this.__isNumber(secondNumber)){
+        this.__secondNumber = secondNumber;
+    }else{ 
+        this.__secondNumber = 0;
+    }
+
+    // 3. Публичное свойство result;
+    this.result = 0;
+}
+
+// 4. Приватный метод __calc(), который записывает в result сумму свойств __firstNumber и __secondNumber;
+Adder.prototype.__calc = function(){
+    this.result = this.__firstNumber + this.__secondNumber;
+}
+
+// 5. методы геттер-сеттер для свойств __firstNumber и __secondNumber
+Adder.prototype.firstNumber = function(firstNumber){
+    if(firstNumber === undefined){
+        return this.__firstNumber;
+    }else {
+        if(this.__isNumber(firstNumber)){
+            this.__firstNumber = firstNumber;
+            this.__calc();
+        }
+    }
+}
+
+Adder.prototype.secondNumber = function(secondNumber){
+    if(secondNumber === undefined){
+        return this.__secondNumber;
+    }else {
+        if(this.__isNumber(secondNumber)){
+            this.__secondNumber = secondNumber;
+            this.__calc();
+        }
+    }
+}
+
+// валидация
+Adder.prototype.__isNumber = function(number){
+    if(typeof number === "number"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var res = new Adder(5, 10);
+
+
+res.secondNumber(5)
+console.log(res.result);
+res.firstNumber(4)
+console.log(res.result);
