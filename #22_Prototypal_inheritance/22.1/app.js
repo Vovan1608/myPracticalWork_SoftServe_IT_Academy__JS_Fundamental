0594a06e61e6362bda7e901e0f47e472812dcf02
Vovan1608 <vovan1608@gmail.com>
2020-09-06 09:50:31 +0300

    solved 22.1

--- #22_Prototypal_inheritance/22.1/app.js
+++ #22_Prototypal_inheritance/22.1/app.js
@@ -41,3 +41,68 @@ GeometricFigure.prototype.setCenterY = function(centerY){
 GeometricFigure.prototype.getCenterX = function(){
     return this.__centerY;
 }
+
+function Rectangle(centerX, centerY, diagonal){
+    GeometricFigure.call(this, centerX, centerY);
+    if(this.__isNumber(diagonal)){
+        this.__diagonal = diagonal;
+    }else{
+        this.__diagonal = 1;
+    }
+}
+
+Rectangle.prototype = Object.create(GeometricFigure.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.setDiagonal = function(diagonal){
+    if(this.__isNumber(this.__diagonal)){
+        this.__diagonal = diagonal;
+    }
+}
+
+Rectangle.prototype.getDiagonal = function(){
+    return this.__diagonal;
+}
+
+Rectangle.prototype.info = function (){
+    console.log("Rectangle: ");
+    console.log("X is: " + this.__centerX);
+    console.log("Y is: " + this.__centerY);
+    console.log("Diagonal is: " + this.__diagonal);
+}
+
+function Circle(centerX, centerY, radius){
+    GeometricFigure.call(this, centerX, centerY);
+    if(this.__isNumber(radius)){
+        this.__radius = radius;
+    }else{
+        this.__radius = 1;
+    }
+}
+
+Circle.prototype = Object.create(GeometricFigure.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.setRadius = function(radius){
+    if(this.__isNumber(this.__radius)){
+        this.__radius = radius;
+    }
+}
+
+Circle.prototype.getRadius = function(){
+    return this.__radius;
+}
+
+Circle.prototype.info = function (){
+    console.log("Circle:");
+    console.log("X is: " + this.__centerX);
+    console.log("Y is: " + this.__centerY);
+    console.log("Radius is: " + this.__radius);
+}
+
+
+var ract = new Rectangle(3, 5, 8);
+ract.info();
+
+var circ = new Circle(8, 9, 10);
+circ.info()
