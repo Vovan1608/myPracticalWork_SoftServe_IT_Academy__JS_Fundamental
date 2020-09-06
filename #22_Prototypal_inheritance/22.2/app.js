d28a9671e24075e31abdf5faac32223b54011e6e
Vovan1608 <vovan1608@gmail.com>
2020-09-06 13:33:57 +0300

    solved task 22.2

--- #22_Prototypal_inheritance/22.2/app.js
+++ #22_Prototypal_inheritance/22.2/app.js
@@ -26,4 +26,40 @@ Square.prototype.getSide = function(){
     return this.__side;
 }
 
+Square.prototype.calcPerimeter = function(){
+    this.perimeter = this.__side * 4;
+}
 
+Square.prototype.getPerimeter = function(){
+    this.calcPerimeter();
+    return this.perimeter;
+}
+
+function Cube(side){
+    Square.call(this, side);
+}
+
+Cube.prototype = Object.create(Square.prototype);
+Cube.prototype.constructor = Cube;
+
+// Без расширения (полностью переопределить метод родителя)
+// Cube.prototype.calcPerimeter = function(){
+//     this.perimeter = this.__side * 12;
+// }
+// var c = new Cube(5);
+// console.log(c.getPerimeter())
+
+
+// С расширением (использовать результат метода родителя для дальнейших расчетов).
+
+Cube.prototype.calcPerimeterCube = function(){
+    this.calcPerimeter();
+    this.perimeter *= 3;
+}
+
+Cube.prototype.getPerimeterCube = function(){
+    this.calcPerimeterCube();
+    return this.perimeter;
+}
+var c2 = new Cube(10);
+console.log(c2.getPerimeterCube())
