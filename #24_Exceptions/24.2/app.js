2acc1ee8436e3a338f1bd74152f10c0d6043c0a9
Vovan1608 <vovan1608@gmail.com>
2020-09-08 17:42:54 +0300

    added 24.2

--- #24_Exceptions/24.2/app.js
+++ #24_Exceptions/24.2/app.js
@@ -1,28 +1,16 @@
 ﻿'use strict';
 
 function sumSliceArray(arr, first, second){
-    var sum = arr[first - 1] + arr[second - 1];
     
     if(typeof first !== "number" || typeof second !== "number"){
         throw new Error ("Введено не число");
     }else if(first > arr.length || second > arr.length || first <= 0 || second <= 0){
         throw new Error ("Число вне диапазона массива");
     }else{
-        return sum;
+        return arr[first - 1] + arr[second - 1];
     }
 }
 
-function check(sumSliceArray){
-    if(
-        typeof first === "number" &&
-        typeof second === "number" &&
-        first <= arr.length &&
-        second <= arr.length &&
-        first > 0 && second > 0
-    ){
-        return ;
-    }
-}
 
-var sum = check(sumSliceArray([2, 4, 5, 7, 10], 1, 5));
+var sum = sumSliceArray([2, 4, 5, 7, 10], 1, 5);
 console.log(sum);
