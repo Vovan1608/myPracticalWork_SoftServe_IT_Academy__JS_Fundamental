4adefef855c99859969f738eb8879d8c5f91ad9c
Vovan1608 <vovan1608@gmail.com>
2020-09-08 12:14:47 +0300

    solved 24.1

--- #24_Exceptions/24.1/app.js
+++ #24_Exceptions/24.1/app.js
@@ -1,11 +1,20 @@
 ﻿'use strict';
 
 function sumSliceArray(arr, first, second){
-    
     var sum = arr[first - 1] + arr[second - 1];
     
-    return sum;
+    if(typeof first !== "number" || typeof second !== "number"){
+        throw new Error ("Введено не число");
+    }else if(first > arr.length || second > arr.length || first <= 0 || second <= 0){
+        throw new Error ("Число вне диапазона массива ");
+    }else{
+        return sum;
+    }
 }
 
-var sum = sumSliceArray([1, 2, 3, 5, 6, 7, 8, 10], 1, 5);
-console.log(sum);
+try{
+    var sum = sumSliceArray([2, 4, 5, 7, 10], 11, 5);
+    console.log(sum);
+}catch (error){
+    console.log(error.message);
+}
