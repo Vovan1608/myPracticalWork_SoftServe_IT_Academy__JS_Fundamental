f2d745a95c7863b0cdc1eccd5e1228e5e2f36cb1
Vovan1608 <vovan1608@gmail.com>
2020-09-08 21:07:13 +0300

    added 25

---
+++ #25_Asynchronous_programming/app.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+function random(min, max, delay, callback) {
+    // Ваш код
+    var error;
+    var num;
+    if(min > max){
+        error = new Error("Invalid parameters");
+    }else{
+        num = parseInt(Math.random() * 100);
+    }
+
+    setTimeout(function(){callback(error, num)}, delay);
+}
+random(1, 100, 2000, function (error, data) {
+    // Ваш код
+    if(error){
+        throw error;
+    }
+    console.log(data)
+});
+random(1000, 100, 3000, function (error, data) {
+    // Ваш код
+    if(error){
+        throw error;
+    }
+    console.log(data)
+});
