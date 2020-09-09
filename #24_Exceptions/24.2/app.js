9d979660bea28a905c4473eac4d0fbaf5636305f
Vovan1608 <vovan1608@gmail.com>
2020-09-09 13:54:05 +0300

    added var arr, first, second

--- #24_Exceptions/24.2/app.js
+++ #24_Exceptions/24.2/app.js
@@ -2,20 +2,28 @@
 
 function sumSliceArray(arr, first, second){
     
-    if(
-        typeof first !== "number" || 
-        typeof second !== "number" || 
-        first > arr.length || 
-        second > arr.length || 
-        first <= 0 || second <= 0
-    ){
-        first = 1;
-        second = arr.length;
+    if(typeof first !== "number" || typeof second !== "number"){
+        throw new Error ("Введено не число");
+    }else if(first > arr.length || second > arr.length || first <= 0 || second <= 0){
+        throw new Error ("Число вне диапазона массива");
+    }else{
+        return arr[first - 1] + arr[second - 1];
     }
-    
-    return arr[first - 1] + arr[second - 1];
 }
 
-var sum = sumSliceArray.apply(null, [[2, 4, 5, 7, 10], 1, 5]);
+var arr = [2, 4, 5, 7, 10];
+var first = 2;
+var second = 5;
+if(
+    typeof first !== "number" ||
+    typeof second !== "number" ||
+    first > arr.length ||
+    second > arr.length ||
+    first <= 0 || second <= 0
+){
+    first = 1;
+    second = arr.length;
+}
 
+var sum = sumSliceArray(arr, first, second);
 console.log(sum);
