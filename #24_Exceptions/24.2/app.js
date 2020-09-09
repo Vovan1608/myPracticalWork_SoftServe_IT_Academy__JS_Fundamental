a07694dea4e2a53c4231dcd51563da36793e80bf
Vovan1608 <vovan1608@gmail.com>
2020-09-09 12:07:01 +0300

    remove throw 24.2

--- #24_Exceptions/24.2/app.js
+++ #24_Exceptions/24.2/app.js
@@ -2,18 +2,21 @@
 
 function sumSliceArray(arr, first, second){
     
-    if(typeof first !== "number" || typeof second !== "number"){
-        throw new Error ("Введено не число");
-    }else if(first > arr.length || second > arr.length || first <= 0 || second <= 0){
-        throw new Error ("Число вне диапазона массива");
+    if(
+        typeof first !== "number" || 
+        typeof second !== "number" || 
+        first > arr.length || 
+        second > arr.length || 
+        first <= 0 || second <= 0
+    ){
+        first = 1;
+        second = arr.length;
+        return arr[first - 1] + arr[second - 1];
     }else{
         return arr[first - 1] + arr[second - 1];
     }
 }
 
-var sum = sumSliceArray([2, 4, 5, 7, 10], 1, 5);
-
-// Я НЕ ЗНАЮ, ЧТО ПИСАТЬ ТУТ. Я не понимаю , 
-// как взять параметрі first и second прям здесь.
+var sum = sumSliceArray.apply(null, [[2, 4, 5, 7, 10], 11, 5]);
 
 console.log(sum);
