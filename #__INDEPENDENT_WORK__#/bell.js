22ae21d68c98e9f10077d6515f367f35c76a7625
Vovan1608 <vovan1608@gmail.com>
2020-09-09 15:24:16 +0300

    changed volumeUp Down

---
+++ #__INDEPENDENT_WORK__#/bell.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+
+function Bell(name){
+    this._name = name;
+    this._power = false;
+    this._volume = 5;
+    this._melody = 1;
+}
+
+Bell.prototype.getName = function(){
+    return this._name;
+}
+
+Bell.prototype.on = function(){
+    this._power = true;
+}
+
+Bell.prototype.off = function(){
+    this._power = false;
+}
+
+Bell.prototype.getPower = function(){
+    return this._power;
+}
+
+Bell.prototype._isNumber = function(number){
+    if(typeof number === "number"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+Bell.prototype.getVolume = function(){
+    return this._volume;
+}
+
+Bell.prototype.volumeUp = function(){
+    ++this._volume;
+}
+
+Bell.prototype.volumeDown = function(){
+    --this._volume;
+}
+
+
+
