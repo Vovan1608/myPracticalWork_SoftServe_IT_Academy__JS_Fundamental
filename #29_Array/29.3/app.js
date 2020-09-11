c5ddacb91b1391c3d6f1cc0c3bd26c113ef56f07
Vovan1608 <vovan1608@gmail.com>
2020-09-11 16:52:44 +0300

    solved 29.3

--- #29_Array/29.3/app.js
+++ #29_Array/29.3/app.js
@@ -1,15 +1,11 @@
 ﻿'use strict';
-
-// Ваш код
-// Array.prototype.sortDesc = function(){
-    
-// }
-
-function sortDesc(){
-    let arr = [];
-    arr.sort().reverse();
-    return arr;
+function deleteEvenNumbers(array) {
+    // Ваш код
+    array = array.filter(function(elem){
+        return (elem % 2 != 0);
+    })
+    return array;
 }
-let arr = [5, 1, 4, 2, 3];
-arr.sortDesc();
-console.log(arr); // [5, 4, 3, 2, 1]
+var arr = [1, 2, 3, 4, 5];
+var arr = deleteEvenNumbers(arr);
+ console.log(arr); // [1, 3, 5]
