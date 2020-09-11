7e646f206bbc4a6fb820a20e4a820bcb1546b2f9
Vovan1608 <vovan1608@gmail.com>
2020-09-11 07:21:56 +0300

    added 27.1

---
+++ #26_Object/app.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+let rectangle = {
+    x1: 1,
+    y1: 2,
+    x2: 6,
+    y2: 8,
+    toString: function(){
+        return (
+            "x1: " + this.x1 + 
+            ", y1: " + this.y1 +  
+            ", x2: " + this.x2 + 
+            ", y2: " + this.y2
+        );
+    },
+    valueOf: function(){
+        let sideA = this.y2 - this.y1;
+        let sideB = this.x2 - this.x1;
+        let p = 2 * (sideA + sideB);
+        return p;
+    } 
+}
---
+++ #27_String/27.1/app.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+let str ="Login1,LOgin2,login3,loGin4";
+
+str = str.toLowerCase().split(",");
+
+console.log(str);
