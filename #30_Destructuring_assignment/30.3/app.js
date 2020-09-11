237d007bc8de6c535c8a2c7858a208ea05f90a68
Vovan1608 <vovan1608@gmail.com>
2020-09-11 20:32:51 +0300

    solved 30.3

---
+++ #30_Destructuring_assignment/30.3/app.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+let data = {
+    names: ["Sam", "Tom", "Ray", "Bob"],
+    ages: [20, 24, 22, 26],
+};
+let {
+    names: [name1, name2, name3, name4], 
+    ages: [age1, age2, age3, age4]
+} = data;
+console.log(name2); // "Tom"
+console.log(age2); // 24
+console.log(name4); // "Bob"
+console.log(age4); // 26
