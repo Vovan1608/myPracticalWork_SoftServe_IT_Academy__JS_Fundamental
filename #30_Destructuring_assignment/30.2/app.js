ce20039a2712baad2d6375dc59fd73b78226e4ca
Vovan1608 <vovan1608@gmail.com>
2020-09-11 20:15:13 +0300

    added 30.2

---
+++ #30_Destructuring_assignment/30.2/app.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+let names = {
+    first: "Tom",
+    second: "Sam",
+    third: "Ray", 
+    fourth: "Bob"
+};
+ let {/* Ваш код */} = names;
+ console.log(f); // "Tom"
+ console.log(x); // "Ray"
+ console.log(fifth); // "Name №5"
