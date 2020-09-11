9f44e88faa9b31e322fd85c7fabbd63056b78ad0
Vovan1608 <vovan1608@gmail.com>
2020-09-11 10:18:13 +0300

    added 29.1

---
+++ #29_Array/29.1/app.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+let musicStyle = ["Джаз", "Блюз"];
+console.log(musicStyle);
+
+musicStyle.push("Рок-н-Ролл");
+console.log(musicStyle);
+
+musicStyle.splice(musicStyle.length - 2, 1, "Классика");
+console.log(musicStyle);
+
+musicStyle.splice(0, 1);
+console.log(musicStyle);
+
+musicStyle.unshift("Рэп", "Регги");
+console.log(musicStyle);
