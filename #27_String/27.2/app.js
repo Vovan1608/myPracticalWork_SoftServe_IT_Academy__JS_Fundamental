ca2470935f4b11e01740133c9cb75c1196ecd926
Vovan1608 <vovan1608@gmail.com>
2020-09-11 09:28:46 +0300

    added 27.2

---
+++ #27_String/27.2/app.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+let str;
+while(str != "end"){
+    str = prompt("Enter the string.", "end");
+    let temp = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str.charCodeAt(i) > 47 && str.charCodeAt(i) < 58){
+            temp++;
+            break;
+        }
+    }
+    if(temp > 0){
+        console.log(str + " Numbers: " + str);
+    }else{
+        console.log(str + " No numbers: " + str);
+    }
+}
