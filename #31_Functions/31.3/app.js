0edb95ae7ae3131d8144976fa598489c0fb960c9
Vovan1608 <vovan1608@gmail.com>
2020-09-12 22:20:29 +0300

    added 31.3

---
+++ #31_Functions/31.3/app.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+let server = {
+    data: 0,
+    convertToString: function (callback) {
+        callback((function () {
+            return this.data + "";
+        }).bind(this));
+    }
+};
+let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+        this.server.data = data;
+        this.server.convertToString(this.notification());
+    },
+    notification: function () {
+        return (function (callback) {
+            this.result = callback();
+        }).bind(this);
+    }
+};
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
