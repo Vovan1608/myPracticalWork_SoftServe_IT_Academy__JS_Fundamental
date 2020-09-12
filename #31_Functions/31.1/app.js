56f085527a238502c7e9c9a4d60f18367f11f477
Vovan1608 <vovan1608@gmail.com>
2020-09-12 21:52:41 +0300

    solved 31.1

--- #31_Functions/31.1/app.js
+++ #31_Functions/31.1/app.js
@@ -1,7 +1,21 @@
 ﻿'use strict';
 
-function mul() {
+function mul(...rest) {
     // Ваш код
+    let res = 1;
+    let count = 0;
+    rest.forEach(function(i){
+        if(typeof rest[i] === "number"){
+            res *= rest[i];
+            count++; 
+        }
+    });
+    if(count){
+        return res;
+    }else{
+        return 0;
+    }
 }
+
 console.log(mul(1, "str", 2, 3, true)); // 6
 console.log(mul(null, "str", false, true)); // 0
