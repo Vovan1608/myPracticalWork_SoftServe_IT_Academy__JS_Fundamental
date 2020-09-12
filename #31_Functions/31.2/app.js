00c65265d029094fb2c701628d02b00d3e16f33d
Vovan1608 <vovan1608@gmail.com>
2020-09-12 22:04:58 +0300

    solved 31.2

---
+++ #31_Functions/31.2/app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+let obj = {
+    name: "obj",
+    print: function () {
+       // Ваш код
+        return  () => {
+            console.log(this.name);
+        }
+    }
+};
+function multiCaller(func, count) {
+    for (let i = 0; i < count; i++) {
+        func();
+    }
+}
+multiCaller(obj.print(), 3);  // "obj", 3 раза
