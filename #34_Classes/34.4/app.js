4b30548e94063e5e87d79f800eb5350853cf77da
Vovan1608 <vovan1608@gmail.com>
2020-09-15 16:26:13 +0300

    added methods copyCircle, static clonCircle

--- #34_Classes/34.4/app.js
+++ #34_Classes/34.4/app.js
@@ -13,10 +13,26 @@ class Circle{
     static calcCircuit(radius){
         return 2 * radius * Math.PI;
     }
-
+    copyCircle(){
+        let copy = Object.assign({}, this);
+        return copy;
+    }
+    static clonCircle(centerX, centerY, radius){
+        let circle = {
+            centerX,
+            centerY,
+            radius
+        }
+        return circle;
+    }
+    isInCircle(x, y){
+        
+    }
 }
 
 let c = new Circle(5, 5, 6);
 console.log(c);
 console.log(c.calcCircumference())
 console.log(Circle.calcCircuit(5))
+console.log(c.copyCircle())
+console.log(Circle.clonCircle(2, 3, 6))
