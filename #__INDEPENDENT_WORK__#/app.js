fd845137b4783757d4f57f8dccb5eb121da3267f
Vovan1608 <vovan1608@gmail.com>
2020-09-15 19:50:51 +0300

    added parent Device

--- #__INDEPENDENT_WORK__#/app.js
+++ #__INDEPENDENT_WORK__#/app.js
@@ -1,35 +1,52 @@
 ﻿'use strict';
 
-function Bell(name){
+function Device(name){
     this._name = name;
     this._power = true;
-    this._volume = 5;
     this._melody = 1;
 }
 
-Bell.prototype.getName = function(){
-    return this._name;
-}
-
-Bell.prototype.on = function(){
-    this._power = true;
-}
-
-Bell.prototype.off = function(){
-    this._power = false;
-}
-
-Bell.prototype.getPower = function(){
-    return this._power;
-}
-
-Bell.prototype._isNumber = function(number){
+Device.prototype._isNumber = function(number){
     if(typeof number === "number"){
         return true;
     }else{
         return false;
     }
 }
+
+Device.prototype.getName = function(){
+    return this._name;
+}
+
+Device.prototype.on = function(){
+    this._power = true;
+}
+
+Device.prototype.off = function(){
+    this._power = false;
+}
+
+Device.prototype.getPower = function(){
+    return this._power;
+}
+
+Device.prototype.getMelody = function(){
+    return this._melody;
+}
+
+Device.prototype.setMelody = function(number){
+    if(this._isNumber(number) && number >=0 && number <= 10){
+        this._melody = number;
+    }
+}
+
+function Bell(name){
+    Device.call(this, name);
+    this._volume = 5;
+}
+
+Bell.prototype = Object.create(Device.prototype);
+Bell.prototype.constructor = Bell;
 
 Bell.prototype.getVolume = function(){
     return this._volume;
@@ -47,47 +64,14 @@ Bell.prototype.volumeDown = function(){
     }
 }
 
-Bell.prototype.getMelody = function(){
-    return this._melody;
-}
-
-Bell.prototype.setMelody = function(number){
-    if(this._isNumber(number) && number >=0 && number <= 10){
-        this._melody = number;
-    }
-}
-
 function Clock(name){
-    this._name = name;
-    this._power = true;
+    Device.call(this, name);
     this._hours = 0;
     this._minutes = 0;
-    this._melody = 1;
 }
 
-Clock.prototype.getName = function(){
-    return this._name;
-}
-
-Clock.prototype.on = function(){
-    this._power = true;
-}
-
-Clock.prototype.off = function(){
-    this._power = false;
-}
-
-Clock.prototype.getPower = function(){
-    return this._power;
-}
-
-Clock.prototype._isNumber = function(number){
-    if(typeof number === "number"){
-        return true;
-    }else{
-        return false;
-    }
-}
+Clock.prototype = Object.create(Device.prototype);
+Clock.prototype.constructor = Clock;
 
 Clock.prototype.setHours = function (number){
     if(this._isNumber(number) && number >=0 && number <= 23){
@@ -105,12 +89,7 @@ Clock.prototype.getTime = function (){
     return (this._hours + ":" + this._minutes);
 }
 
-Clock.prototype.getMelody = function(){
-    return this._melody;
-}
-
-Clock.prototype.setMelody = function(number){
-    if(this._isNumber(number) && number >=0 && number <= 10){
-        this._melody = number;
-    }
-}
+let b = new Bell("sony")
+console.log(b)
+let c = new Clock("clock")
+console.log(c)
