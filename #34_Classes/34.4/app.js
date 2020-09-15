8168268982f93fc36fb2443ec564a59ad4c3a145
Vovan1608 <vovan1608@gmail.com>
2020-09-15 13:08:17 +0300

    added 34.4

---
+++ #34_Classes/34.4/app.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+class Circle{
+    constructor(centerX, centerY, radius){
+        this._centerX = centerX;
+        this._centerY = centerY;
+        this._radius = radius;
+    }
+    calcCircumference(){
+        let circumference = 2 * this._radius * Math.PI;
+        return circumference;
+    }
+    static calcCircuit(radius){
+        return 2 * radius * Math.PI;
+    }
+
+}
+
+let c = new Circle(5, 5, 6);
+console.log(c);
+console.log(c.calcCircumference())
+console.log(Circle.calcCircuit(5))
