054f4c5f22cb43bf37974475e0ffad45e15c5b37
Vovan1608 <vovan1608@gmail.com>
2020-09-15 07:40:27 +0300

    solved 33.2

---
+++ #33_Objects/33.2/app.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+let mp3 = {
+    volume: 1,
+    trek: 1,
+    volIncrease() {
+        this.volume++;
+    },
+    volDown(){
+        this.volume--;
+    },
+    forvard(){
+        this.trek++;
+    },
+}
