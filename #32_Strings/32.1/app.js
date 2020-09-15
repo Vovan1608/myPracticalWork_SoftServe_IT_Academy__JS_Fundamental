8c5ed745c4d6b64da44fcf4bb758750db9cd6574
Vovan1608 <vovan1608@gmail.com>
2020-09-15 07:05:58 +0300

    solved 32

---
+++ #32_Strings/32.1/app.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+let names = {
+    first: "Tom",
+    second: "Sam",
+    third: "Ray"
+};
+console.log(`Первый: -${names.first}-, Второй - "${names.second}", Третий => '${names.third}'`); 
+// "Первый: -Tom-, Второй - "Sam", Третий => 'Ray'"
