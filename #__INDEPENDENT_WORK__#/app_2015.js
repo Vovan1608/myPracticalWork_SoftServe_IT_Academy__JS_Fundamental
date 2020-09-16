2a24d7f1f04a67dbb8e3db839017a851f46dea52
Vovan1608 <vovan1608@gmail.com>
2020-09-16 16:27:56 +0300

    added app_2015.js

---
+++ #__INDEPENDENT_WORK__#/app_2015.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+class Device{
+    constructor(name){
+        this._name = name;
+        this._power = true;
+        this._melody = 1;
+    }
+    static isNumber(number){
+        if(typeof number === "number"){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    get name(){
+        return this._name
+    }
+    on(){
+        this._power = true;
+    }
+    off(){
+        this._power = false;
+    }
+    get power(){
+        return this._power;
+    }
+    get melody(){
+        return this._melody;
+    }
+    set melody(number){
+        if(this._isNumber(number) && number >=0 && number <= 10){
+            this._melody = number;
+        }
+    }
+}
+
+class Bell extends Device{
+    constructor()
+}
