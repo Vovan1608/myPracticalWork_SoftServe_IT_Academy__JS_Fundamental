36441a0d4b1bf7eb9614e72bdc6e399775c9bd54
Vovan1608 <vovan1608@gmail.com>
2020-09-29 13:38:06 +0300

    changed 35.4

---
+++ #35_BOM_DOM/35.5/app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+/*
+Для страницы:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Свойства узлов</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <div><b>Привет мир!</b></div>
+        <div><i>Привет мир!</i></div>
+        <script>
+            // Ваш код
+        </script>
+    </body>
+</html>
+1. Поменяйте местами содержимое элементов <div>.
+
+*/
+
