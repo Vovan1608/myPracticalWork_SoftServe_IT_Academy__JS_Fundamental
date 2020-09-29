715f53dffd6c9122ef58f4c6871d973f01ca6268
Vovan1608 <vovan1608@gmail.com>
2020-09-29 21:00:16 +0300

    solved 35.11

---
+++ #35_BOM_DOM/35.11/app.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+/*
+Напишите функцию, которая удаляет элемент из DOM. Синтаксис должен быть таким: remove(elem), то есть, в отличие от parentNode.removeChild(elem) – без родительского элемента parentNode.
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Удаление и замена узлов</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <div>Это</div>
+        <div>Все</div>
+        <div>Элементы DOM</div>
+        <script>
+            function remove(elem) { 
+                // Ваш код
+            }
+            var e = document.body.children[0];
+            remove(e); // Удаление элемента
+        </script>
+    </body>
+</html>
+
+*/
+
