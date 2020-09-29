3dc0b20388fd4ff6a7cb46ef5b4f449a146d5171
Vovan1608 <vovan1608@gmail.com>
2020-09-29 12:58:37 +0300

    added 35.3

---
+++ #35_BOM_DOM/35.3/app.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+/*
+Для страницы:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Навигация в DOM</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <form name="authForm">
+            Логин:<br />
+            <input type="text" name="log" /><br />
+            Пароль:<br />
+            <input type="text" name="pass" /><br />
+            <button name="signIn">Войти</button>
+        </form>
+        <script>
+            // Ваш код
+        </script>
+    </body>
+</html>
+1. Получите форму по имени в переменную;
+2. Получите форму по номеру в переменную;
+3. Получите первый элемент <input> в переменную;
+4. Получите элемент <button> в переменную;
+5. Выведите значения переменных на экран.
+*/
+
