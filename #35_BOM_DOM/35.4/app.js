38b16a273a063f3b7b8040219bcb1803259504dc
Vovan1608 <vovan1608@gmail.com>
2020-09-29 13:21:22 +0300

    added 35.4

---
+++ #35_BOM_DOM/35.4/app.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+/*
+Для страницы:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Свойства узлов</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <div>Блок текста №1</div>
+        <p>Абзац №1</p>
+        <p>Абзац №2</p>
+        <div>Блок текста №2</div>
+        <p>Абзац №3</p>
+        <script>
+            var allElements = document.body.children;
+            var pElements = [];
+            // Ваш код
+            alert(pElements.length); // Должно вывести 3
+        </script>
+    </body>
+</html>
+1. Используя свойство tagName, выберите из массива allElements 
+все элементы <p> и запишите их в массив pElements.
+*/
+
