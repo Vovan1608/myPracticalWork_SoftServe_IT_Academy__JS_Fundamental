3ded2f51ec1883798b10a4f5613b0b7e5ea65a7c
Vovan1608 <vovan1608@gmail.com>
2020-09-29 15:38:25 +0300

    added 35.7

---
+++ #35_BOM_DOM/35.7/app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+/*
+Для страницы:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Атрибуты узлов</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <div data-widget-name="menu">Выберите жанр</div>
+        <script>
+            // Ваш код
+        </script>
+    </body>
+</html>
+1. Получите элемент <div> в переменную;
+2. Получите значение атрибута data-widget-name в переменную;
+3. Выведите значения переменных на экран.
+*/
+
