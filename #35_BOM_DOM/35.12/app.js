28be7ab737092a5ce6f460c7ef97c123627e6ffa
Vovan1608 <vovan1608@gmail.com>
2020-09-29 21:23:48 +0300

    added 35.12

---
+++ #35_BOM_DOM/35.12/app.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+/*
+Для страницы:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>События</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <p id="color">Текст</p>
+        <button>Красный</button>
+        <button>Синий</button>
+        <button>Сброс</button>
+        <script>
+            // Ваш код
+        </script>
+    </body>
+</html>
+1. Сделайте так, чтобы при клике на кнопки, изменялся цвет 
+текста элемента с идентификатором "color". Пример:
+
+После нажатия на кнопку “Красный”:
+
+
+*/
+
