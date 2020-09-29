754aa57b9a8fcd609dc4f689697177c205036d40
Vovan1608 <vovan1608@gmail.com>
2020-09-29 20:51:37 +0300

    added 35.10

---
+++ #35_BOM_DOM/35.10/app.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+/*
+Напишите функцию insertAfter(elem, refElem), которая добавит 
+elem после узла refElem.
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Добавление узлов</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <div>Это</div>
+        <div>Элементы</div>
+        <script>
+            function insertAfter(elem, refElem) {
+                // Ваш код
+            }
+            var e = document.createElement("div");
+            e.innerHTML = "<b>Новый элемент</b>";
+            var b = document.body;
+            insertAfter(e, b.firstChild); // Вставка после первого элемента
+            insertAfter(e, b.lastChild); // Вставка за последним элементом
+        </script>
+    </body>
+</html>
+*/
+
