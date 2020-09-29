524b66db5db8f36b4dfd474a8784a23ca03d50a3
Vovan1608 <vovan1608@gmail.com>
2020-09-29 17:32:40 +0300

    solved 35.9

---
+++ #35_BOM_DOM/35.9/app.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+/*
+Для страницы:
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Создание узлов. Добавление узлов</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <form>
+            Имя: <input type="text" name="name" /><br />
+            Возраст: <input type="text" name="age" /><br />
+        </form>
+        <script>
+            // Ваш код
+        </script>
+    </body>
+</html>
+1. Создайте кнопку для отправки данных формы;
+2. Добавьте созданную кнопку в конец формы.
+
+*/
+
