36e7a2bf3e2a3d7356d754e01d6d5395b92ac218
Vovan1608 <vovan1608@gmail.com>
2020-09-30 15:04:28 +0300

    solved 37

---
+++ #37_AJAX/37.1/app.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+/*
+Создайте цепочку из трех запросов (т.е. следующий запрос должен 
+    отправляться, только после того, как выполнится текущий) при 
+    помощи Fetch:
+1. Получите данные по url: https://reqres.in/api/users/5;
+2.Создайте данные c произвольными значениями по url: https://reqres.in/api/users/;
+3. Получите данные по url: https://reqres.in/api/users/10.
+*/
+
+
+fetch("https://reqres.in/api/users/5")
+    .then((response) => {
+        return response.json();
+    })
+    .then( (data) => {
+        return fetch("https://reqres.in/api/users/", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    })
+    .then((response) => {
+        return fetch("https://reqres.in/api/users/10")
+    })
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err))
