6762997b8d6b91bebd16d1007eeebc0d00450248
Vovan1608 <vovan1608@gmail.com>
2020-09-30 13:36:42 +0300

    solved 36.2

---
+++ #36_Promise/36.2/app.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+/*
+Создайте следующую асинхронную цепочку promise:
+new Promise(function (resolve, reject) {
+   // Запрашиваем у пользователя число number при помощи prompt()
+   // Если пользователь ввел не число - вызвать reject()
+   // Если пользователь ввел число - вызвать resolve(number)
+}).catch(function (error) {
+    return new Promise(function (resolve, reject) {
+      // Запрашиваем у пользователя число number, пока он его не введет
+      // После ввода числа - вызвать resolve(number)
+    });
+}).then(function (result) {
+   // Вывод number на экран
+});
+*/
+
+new Promise(function (resolve, reject) {
+    
+    // Запрашиваем у пользователя число number при помощи prompt()
+    let number = prompt("Enter number.", "");
+    
+    // Если пользователь ввел не число - вызвать reject()
+    if(isNaN(number) || number === null || number === ""){
+        return reject (error);
+    }else{
+    
+        // Если пользователь ввел число - вызвать resolve(number)
+        resolve(number);
+    }
+}).catch(function (error) {
+    return new Promise(function (resolve, reject) {
+        
+        // Запрашиваем у пользователя число number, пока он его не введет
+        do{
+            var number = prompt("Enter number!","5");
+        }while (isNaN(number) || number === null || number === " ");
+        
+        // После ввода числа - вызвать resolve(number)
+        resolve(number);
+    });
+}).then(function (result) {
+    // Вывод number на 
+    console.log(result);
+});
